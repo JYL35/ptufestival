f922e38fdc78d468c430dfcabf333170eb5d60c6
JYL35 <dlwodbs1223@naver.com>
2025-03-16 21:32:34 +0900

    Create Spring Boot project

---
+++ src/main/java/com/capstone7/ptufestival/PtufestivalApplication.java
@@ -0,0 +1,13 @@
+package com.capstone7.ptufestival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtufestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtufestivalApplication.class, args);
+	}
+
+}
